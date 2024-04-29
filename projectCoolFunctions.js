4febecac8601eadec6f129546505e062a327b22d
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-04-29 15:09:37 -0300

    added reverseString + tests

--- projectCoolFunctions.js
+++ projectCoolFunctions.js
@@ -7,3 +7,16 @@ export function capitalize(word) {
     if (word.length == 0) return "";
     return word[0].toLocaleUpperCase() + word.slice(1, word.length);
 }
+
+/**
+ * takes a string and returns it reversed
+ * @param {string} string the string to be reversed
+ * @returns {string} a new reversed string
+ */
+export function reverseString(string) {
+    let newString = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        newString += string[i];
+    }
+    return newString;
+}
--- projectCoolFunctions.test.js
+++ projectCoolFunctions.test.js
@@ -1,4 +1,4 @@
-import { capitalize } from "./projectCoolFunctions";
+import { capitalize, reverseString } from "./projectCoolFunctions";
 import { test, describe, expect } from "@jest/globals";
 
 describe("test capitalize function", () => {
@@ -8,5 +8,18 @@ describe("test capitalize function", () => {
 
     test("Should return empty string if the input is empty", () => {
         expect(capitalize("")).toBe("");
+    });
+});
+describe("test reverseString function", () => {
+    test("reversing a string", () => {
+        expect(reverseString("eduarte")).toBe("etraude");
+    });
+
+    test("reversing the same string two times", () => {
+        expect(reverseString(reverseString("hello world"))).toBe("hello world");
+    });
+
+    test("empty string case", () => {
+        expect(reverseString("")).toBe("");
     });
 });
